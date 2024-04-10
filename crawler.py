1b4b955ad07b47778841a1e995234ef86201b7bd
Billy Lights <alpha5611331@gmail.com>
2024-04-10 18:58:13 +0900

    Rostock ...

--- crawler.py
+++ crawler.py
@@ -29,9 +29,33 @@ DONE_MARKER_NAME = "done"
 
 CHROME = None
 
-HOME_URL = "https://www.hwk-mittelfranken.de/betriebe/suche-75,0,bdbsearch.html?search-searchterm=&search-filter-zipcode=90402&search-filter-radius=250&search-filter-jobnr=&search-job=&search-local=&search-filter-training=&search-filter-experience="
-TOTAL_PAGES = 1439
+# Nürnberg
+# HOME_URL = "https://www.hwk-mittelfranken.de/betriebe/suche-75,0,bdbsearch.html?search-searchterm=&search-filter-zipcode=90402&search-filter-radius=250&search-filter-jobnr=&search-job=&search-local=&search-filter-training=&search-filter-experience="
+# TOTAL_PAGES = 1439
+# PAGE_SIZE = 10
+
+# Rostock
+HOME_URL = "https://www.hwk-omv.de/betriebe/suche-18,57,bdbsearch.html?search-searchterm=&search-filter-zipcode=Rostock&search-filter-radius=250&search-filter-jobnr=&search-job=&search-local=&search-filter-training=&search-filter-experience="
+TOTAL_PAGES = 587
 PAGE_SIZE = 10
+
+# Köln
+# HOME_URL = "https://www.hwk-koeln.de/betriebe/suche-32,0,bdbsearch.html?search-searchterm=&search-filter-zipcode=50667&search-filter-radius=250&search-filter-jobnr=&search-job=&search-local=&search-filter-experience="
+# TOTAL_PAGES = 2716
+# PAGE_SIZE = 10
+
+
+def gen_page_url(page_index: int) -> str:
+    # Nürnberg
+    # page_link = f"https://www.hwk-mittelfranken.de/betriebe/suche-75,0,bdbsearch.html?search-searchterm=&search-job=&search-local=&search-filter-zipcode=90402&search-filter-latitude=49.453333&search-filter-longitude=11.091667&search-filter-radius=250&search-filter-jobnr=&search-filter-training=&search-filter-experience=&limit={PAGE_SIZE}&offset={page_index * PAGE_SIZE}"
+
+    # Rostock
+    page_link = f"https://www.hwk-omv.de/betriebe/suche-18,57,bdbsearch.html?limit={PAGE_SIZE}&search-searchterm=&search-job=&search-local=&search-filter-zipcode=Rostock&search-filter-radius=250&search-filter-jobnr=&search-filter-training=&search-filter-experience=&offset={page_index * PAGE_SIZE}"
+
+    # Köln
+    # page_link = f"https://www.hwk-koeln.de/betriebe/suche-32,0,bdbsearch.html?limit={PAGE_SIZE}&search-searchterm=&search-job=&search-local=&search-filter-zipcode=50667&search-filter-latitude=50.941389&search-filter-longitude=6.953611&search-filter-radius=250&search-filter-jobnr=&search-filter-experience=&offset={page_index * PAGE_SIZE}"
+
+    return page_link
 
 
 def mark_as_done(dir_path: str):
@@ -238,8 +262,7 @@ def work(start: int, count: int):
         USER_AGENT = CHROME.run_script("navigator.userAgent")
 
         for i in range(begin_page, end_page):
-            page_link = f"https://www.hwk-mittelfranken.de/betriebe/suche-75,0,bdbsearch.html?search-searchterm=&search-job=&search-local=&search-filter-zipcode=90402&search-filter-latitude=49.453333&search-filter-longitude=11.091667&search-filter-radius=250&search-filter-jobnr=&search-filter-training=&search-filter-experience=&limit={PAGE_SIZE}&offset={i * 10}"
-            crawl_page(page_index=i, page_link=page_link)
+            crawl_page(page_index=i, page_link=gen_page_url(i))
 
         CHROME.quit()
         log_inf("All done.")
